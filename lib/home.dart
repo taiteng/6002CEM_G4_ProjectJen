a5c253e3e6ce066775fe6516ecc33f2bc770b8d8
esoon <p20012522@student.newinti.edu.my>
2023-05-11 01:14:00 +0800

    Display the properties in list view. Yet to enhance with proper image and design.

--- lib/home.dart
+++ lib/home.dart
@@ -13,133 +13,125 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
-  CollectionReference _property = FirebaseFirestore.instance.collection('Property');
-
-  Future getPropertyList() async{
-    QuerySnapshot querySnapshot = await _property.get();
-    final propertyDataList = querySnapshot.docs.map((doc) => doc.data()).toList();
-
-    print(propertyDataList);
-  }
+  final Stream<QuerySnapshot> _property = FirebaseFirestore.instance.collection('Property').snapshots();
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: FutureBuilder<DocumentSnapshot>(
-            future: _property.get(),
-            builder: (BuildContext context,
-                AsyncSnapshot<DocumentSnapshot> snapshot) {
-              if (snapshot.hasError) {
-                return Center(child: Text(snapshot.error.toString()));
-              } else if (snapshot.connectionState == ConnectionState.done) {
-                Map<String, dynamic> data =
-                    snapshot.data!.data() as Map<String, dynamic>;
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
 
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 30,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ConstrainedBox(
-                            constraints:
-                                BoxConstraints.tightFor(width: 100, height: 40),
-                            child: OutlinedButton(
-                                onPressed: () {},
-                                child: Text(
-                                  "BUY",
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                  ),
-                                ) //BUY Button
-                                ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ConstrainedBox(
+                    constraints:
+                    BoxConstraints.tightFor(width: 100, height: 40),
+                    child: OutlinedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "BUY",
+                          style: TextStyle(
+                            fontSize: 15,
                           ),
-                          ConstrainedBox(
-                            constraints:
-                                BoxConstraints.tightFor(width: 100, height: 40),
-                            child: ElevatedButton(
-                                onPressed: () {},
-                                child: Text(
-                                  "SELL",
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                  ),
-                                ) //BUY Button
-                                ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      ConstrainedBox(
-                        constraints: BoxConstraints(minWidth: double.infinity),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            TextField(
-                              decoration: InputDecoration(
-                                contentPadding: EdgeInsets.symmetric(),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  borderSide: BorderSide(width: 0.8),
-                                ),
-                                hintText: "E.g. Penang Condominium",
-                                prefixIcon: Icon(
-                                  Icons.search,
-                                  size: 30,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Divider(
-                        color: Colors.grey,
-                        thickness: .2,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Recent Properties",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        height: 160,
-                        width: MediaQuery.of(context).size.width,
-                        padding: EdgeInsets.only(left: 12, right: 12),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                    ],
+                        ) //BUY Button
+                    ),
                   ),
-                );
-              }
-              else {
-                return const Text("loading");
-              }
-            }
-            ),
+                  ConstrainedBox(
+                    constraints:
+                    BoxConstraints.tightFor(width: 100, height: 40),
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "SELL",
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ) //BUY Button
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              ConstrainedBox(
+                constraints: BoxConstraints(minWidth: double.infinity),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(width: 0.8),
+                        ),
+                        hintText: "E.g. Penang Condominium",
+                        prefixIcon: Icon(
+                          Icons.search,
+                          size: 30,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Divider(
+                color: Colors.grey,
+                thickness: .2,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Recent Properties",
+                    style: TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              StreamBuilder(
+                stream: _property,
+                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+                  if(snapshot.hasError){
+                    return Text('Something went wrong');
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Text("Loading");
+                  }
+
+                  return ListView(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                      Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+                      return ListTile(
+                        title: Text(data['name']),
+                        subtitle: Text(data['location']),
+                      );
+                    }).toList(),
+                  );
+                }
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
